53a35be7053c3ea9d733441933631c260ec420fe
Aspid <FranchukAndrei@Gmail.com>
2020-05-16 15:29:25 +0300

    Initial Commit

---
+++ KeyboardEdges/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KeyboardEdges
+//
+//  Created by Франчук Андрей on 16.05.2020.
+//  Copyright © 2020 Франчук Андрей. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
